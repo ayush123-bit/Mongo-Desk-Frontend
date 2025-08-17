bbd071951ec5a739cf95b892d14e150baeb66711
Ayush Rai <145470522+ayush123-bit@users.noreply.github.com>
2025-08-17 15:15:31 +0530

    Update api.js

--- app/lib/api.js
+++ app/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://mango-desk-backend-unap.vercel.app/api";
+const API_BASE_URL = "https://mango-desk-backend-production.up.railway.app/api";
 
 export const generateSummary = async (transcript, prompt, file = null) => {
   try {
